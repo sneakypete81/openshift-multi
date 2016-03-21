4c335650e717d191b35c745021db51f9def3fe81
sneakypete81 <sneakypete81@gmail.com>
2016-03-21 13:15:19 +0000

    Initial attempt at subdomain routing

---
+++ subdomain_dispatcher.py
@@ -0,0 +1,24 @@
+# From http://flask.pocoo.org/docs/0.10/patterns/appdispatch/
+from threading import Lock
+
+class SubdomainDispatcher(object):
+    def __init__(self, domain, create_app):
+        self.domain = domain
+        self.create_app = create_app
+        self.lock = Lock()
+        self.instances = {}
+
+    def get_application(self, host):
+        host = host.split(':')[0]
+        assert host.endswith(self.domain), 'Configuration error'
+        subdomain = host[:-len(self.domain)].rstrip('.')
+        with self.lock:
+            app = self.instances.get(subdomain)
+            if app is None:
+                app = self.create_app(subdomain)
+                self.instances[subdomain] = app
+            return app
+
+    def __call__(self, environ, start_response):
+        app = self.get_application(environ['HTTP_HOST'])
+        return app(environ, start_response)
