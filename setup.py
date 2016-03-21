7b3dfa25569274a0f36a575b63fa4b1e1086ca0c
sneakypete81 <sneakypete81@gmail.com>
2016-03-21 14:15:11 +0000

    Update setup.py details

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+setup(name='sneakypete81-multi',
+      version='0.1',
+      description='OpenShift wrapper around multiple Flask apps',
+      author='Pete Burgers',
+      author_email='sneakypete81@gmail.com',
+      url='http://sneakypete81.github.io/',
 #      install_requires=['Django>=1.3'],
      )
